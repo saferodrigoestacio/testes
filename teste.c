caa290e264b3c1d84dc6d65ba9158668b5fdf103
saferodrigoestacio <202501270689@alunos.estacio.br>
2025-04-24 00:41:30 +0000

    ajustes

--- teste.c
+++ teste.c
@@ -2,13 +2,14 @@
 
 int main() {
     printf("Hello, World!\n");
-
+    printf("\n");
     // while
     int num = 0;
     while(num <= 10) {
         printf("num = %d\n", num);
         num++;
     }
+    printf("\n");
 
     // do while
     int num2 = 0;
@@ -16,11 +17,13 @@ int main() {
         printf("num2 = %d\n", num2);
         num2++;
     } while(num2 <= 10);
+    printf("\n");
 
     // for
     for(int i = 0; i < 5; i++) {
         printf("i = %d\n", i);
     }
+    printf("\n");
 
     return 0;
 }
