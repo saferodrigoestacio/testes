782f56b39dd107307e69ec5f42215bf46fd6339f
saferodrigoestacio <202501270689@alunos.estacio.br>
2025-04-24 01:03:50 +0000

    add code

--- teste.c
+++ teste.c
@@ -5,24 +5,44 @@ int main() {
     printf("\n");
 
     // while
+    printf("ESTRUTURA WHILE...\n");
     int num = 0;
     while(num <= 10) {
-        printf("num = %d\n", num);
+        
+        if (num % 2 == 0) {
+            printf("o número %d é par! \n", num);
+        }
+
         num++;
     }
     printf("\n");
 
     // do while
-    int num2 = 0;
+    printf("ESTRUTURA DO-WHILE...\n");
+    int num2;
     do {
-        printf("num2 = %d\n", num2);
-        num2++;
-    } while(num2 <= 10);
+        printf("Digite um número par para sair do programa... \n");
+        scanf("%d", &num2);
+
+        if (num2 % 2 == 0) {
+            printf("%d é par! \n", num2);
+        } else {
+            printf("%d é ímpar! \n", num2);
+        }
+
+    } while(num2 % 2 != 0);
+    printf("Você digitou um número par! \n");
     printf("\n");
 
     // for
-    for(int i = 0; i <= 5; i++) {
-        printf("i = %d\n", i);
+    printf("ESTRUTURA FOR...\n");
+    int numero, i;
+
+    printf("Digite um número para calcularmos a tabuada... \n");
+    scanf("%d", &numero);
+
+    for(i = 0; i <= 10; i++) {
+        printf("%d x %d = %d \n", numero, i, numero * i);
     }
     printf("\n");
 
